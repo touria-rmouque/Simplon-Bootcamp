a543d03c9e362762b4a7ba94ba225b9217ac881a
touria-rmouque <touriarmouque@gmail.com>
2026-03-11 12:57:07 +0000

    Challenge 2 (SQL) Done

---
+++ MySql-autoformation/Challenge2.sql
@@ -0,0 +1,10 @@
+INSERT INTO library_books (title, author, published_year, status, price) 
+VALUES ('The Lion, the Witch and the Wardrobe', 'C.S. Lewis' , 1950, 'Available', 150.99);
+INSERT INTO library_books (title, author, published_year, status, price) 
+VALUES ('Clean Code', 'Robert C. Martin', 2008, 'Borrowed', 290.99),
+('Clean Architecture', 'Robert C. Martin', 2017, 'Available', 310.50),
+('The Pragmatic Programmer', 'Andrew Hunt', 1999, 'Available', 250.50),
+('Refactoring', 'Martin Fowler', 2018, 'Lost', 350.00),
+('JavaScript: The Good Parts', 'Douglas Crockford', 2008, 'Available', NULL),
+('Introduction to Algorithms', 'Thomas H. Cormen', 2009, 'Lost', 450.00),
+('Design Patterns', 'Erich Gamma', 1994, 'Borrowed', 320.75);
