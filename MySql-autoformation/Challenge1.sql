8bdae93fe3617376a566fb9b95b44acff4b02996
touria-rmouque <touriarmouque@gmail.com>
2026-03-11 12:56:52 +0000

    Challenge 1 (SQL) Done

---
+++ MySql-autoformation/Challenge1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE library_books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    author VARCHAR(100),
+    published_year YEAR,
+    status ENUM('Available','Borrowed','Lost') DEFAULT 'Available',
+    price DECIMAL(6,2)
+);
