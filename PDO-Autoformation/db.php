04fc4a56fe2329885108f1cdf297297a0206848c
touria-rmouque <touriarmouque@gmail.com>
2026-03-13 11:08:53 +0000

    Challenge 7 (PDO) Done

--- PDO-Autoformation/db.php
+++ PDO-Autoformation/db.php
@@ -10,7 +10,7 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 echo"Connected Successfully";
 }
 catch(PDOException $e){
-echo"Connection failed" . $e->getMessage();
+die("Connection failed". $e->getMessage());
 }
 
 ?>
