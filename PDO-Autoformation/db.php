4d1effcea1cf26672ca6519cd5524b27435df1b0
touria-rmouque <touriarmouque@gmail.com>
2026-03-13 11:04:10 +0000

    Challenge 7 (PDO) Done

---
+++ PDO-Autoformation/db.php
@@ -0,0 +1,16 @@
+<?php 
+$servername= "localhost";
+$username= "root";
+$password= "";
+$dbname= "library";
+
+try{
+$conn=new PDO("mysql:host=$servername;port=3307;dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo"Connected Successfully";
+}
+catch(PDOException $e){
+echo"Connection failed" . $e->getMessage();
+}
+
+?>
