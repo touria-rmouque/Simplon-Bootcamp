50b1e779707195413b4f94fdd651d0af45999017
touria-rmouque <touriarmouque@gmail.com>
2026-03-16 14:30:19 +0000

    creation du classe database

---
+++ IT-Asset-Tracker/classes/Database.php
@@ -0,0 +1,27 @@
+<?php
+
+class Database {
+    private $host = "localhost";
+    private $port = "3307";
+    private $db_name = "hardware_inventory";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name, $this->username, $this->password);
+    
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->exec("set names utf8"); 
+
+        } catch(PDOException $exception) {
+            echo "Erreur de connexion : " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
