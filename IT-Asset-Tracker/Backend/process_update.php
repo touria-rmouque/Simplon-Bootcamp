d7550b90828c1be0856210b717f34c39d4bdf2f4
touria-rmouque <touriarmouque@gmail.com>
2026-03-16 15:21:44 +0000

    creation du backend du form update

---
+++ IT-Asset-Tracker/Backend/process_update.php
@@ -0,0 +1,24 @@
+<?php
+require_once '../classes/Database.php';
+require_once '../classes/Asset.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $asset = new Asset(
+        $db, 
+        $_POST['serial_number'], 
+        $_POST['device_name'], 
+        $_POST['price'], 
+        $_POST['status'], 
+        $_POST['category_id']
+    );
+
+    if ($asset->update()) {
+        header("Location: ../Frontend/inventory.php?msg=updated");
+    } else {
+        echo "Erreur lors de la mise à jour.";
+    }
+}
+?>
